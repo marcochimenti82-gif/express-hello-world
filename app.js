e66f02d05140d5f5d8a01434ea6eeaa4b58bf4c0
marcochimenti82-gif <marcochimenti82@gmail.com>
2026-01-09 08:55:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -409,6 +409,10 @@ function forwardToHumanTwiml() {
   const vr = buildTwiml();
   sayIt(vr, t("step9_fallback_transfer_operator.main"));
  vr.dial({ timeout: 20 }, OPERATOR_PHONE);
+}if (isValidPhoneE164(OPERATOR_PHONE)) {
+    vr.dial({ timeout: 20 }, OPERATOR_PHONE);
+  }
+  return vr.toString();
 }
 
 function buildFallbackEmailPayload(session, req, reason) {
